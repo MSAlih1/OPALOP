b32f604f8c8b65d3ca2ac4f503aaac831a94124b
MSAlih1 <aslim@outlook.com.tr>
2016-02-25 00:10:32 +0200

    no message

--- ClusterServer/QPS_Method1/_QPS/Class/ImgAvgColor.cs
+++ ClusterServer/QPS_Method1/_QPS/Class/ImgAvgColor.cs
@@ -7,24 +7,24 @@ using System.Web;
 
 namespace QPS_Web1._QPS.Class
 {
-    public class ImageAvgColor
-    {
-        public int IAvgRgb { get; set; }
-        private Image myVar;
+    //public class ImageAvgColor
+    //{
+    //    public int IAvgRgb { get; set; }
+    //    private Image myVar;
 
-        public Image IImage
-        {
-            get
-            {
-                return myVar;
-            }
-            set { myVar = value; }
-        }
+    //    public Image IImage
+    //    {
+    //        get
+    //        {
+    //            return myVar;
+    //        }
+    //        set { myVar = value; }
+    //    }
 
-        public ImageAvgColor(Image _img)
-        {
-            IImage = _img;
-            IAvgRgb = ImageProperty.GetImageAVGRgb((Bitmap)IImage);
-        }
-    }
+    //    public ImageAvgColor(Image _img)
+    //    {
+    //        IImage = _img;
+    //        IAvgRgb = ImageProperty.GetImageAVGRgb((Bitmap)IImage);
+    //    }
+    //}
 }
